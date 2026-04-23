914bc3c564215b02ae66a2213b3cc14f9d2b808f
1061568-art <1061568@bkrnet.de>
2026-04-23 08:30:48 +0200

    Pokemon Werte überarbeitet

--- pokemonQuartett/MainWindow.xaml.cs
+++ pokemonQuartett/MainWindow.xaml.cs
@@ -107,7 +107,7 @@ namespace pokemonQuartett
         private List<Pokemon> ErstellePokemonListe()
         {
             List<Pokemon> liste = new List<Pokemon>();
-            // Gen 1: Kanto
+            // Gen 1: Kanto (Ausgeglichen)
             liste.Add(new Pokemon { Name = "Bisasam", Typ = "Pflanze", HP = 45, Attack = 49, Defense = 49, Aspeed = 45, BildPfad = "bisasam.png" });
             liste.Add(new Pokemon { Name = "Glumanda", Typ = "Feuer", HP = 39, Attack = 52, Defense = 43, Aspeed = 65, BildPfad = "glumanda.png" });
             liste.Add(new Pokemon { Name = "Schiggy", Typ = "Wasser", HP = 44, Attack = 48, Defense = 65, Aspeed = 43, BildPfad = "schiggy.png" });
@@ -117,10 +117,10 @@ namespace pokemonQuartett
             liste.Add(new Pokemon { Name = "Feurigel", Typ = "Feuer", HP = 39, Attack = 52, Defense = 43, Aspeed = 65, BildPfad = "feurigel.png" });
             liste.Add(new Pokemon { Name = "Karnimani", Typ = "Wasser", HP = 50, Attack = 65, Defense = 64, Aspeed = 43, BildPfad = "karnimani.png" });
 
-            // Gen 3: Hoenn
+            // Gen 3: Hoenn (Spezialisiert)
             liste.Add(new Pokemon { Name = "Geckarbor", Typ = "Pflanze", HP = 40, Attack = 45, Defense = 35, Aspeed = 70, BildPfad = "geckarbor.png" });
             liste.Add(new Pokemon { Name = "Flemmli", Typ = "Feuer", HP = 45, Attack = 60, Defense = 40, Aspeed = 45, BildPfad = "flemmli.png" });
-            liste.Add(new Pokemon { Name = "Hydropi", Typ = "Wasser", HP = 50, Attack = 70, Defense = 50, Aspeed = 40, BildPfad = "hydropi.png" });
+            liste.Add(new Pokemon { Name = "Hydropi", Typ = "Wasser", HP = 70, Attack = 70, Defense = 50, Aspeed = 15, BildPfad = "hydropi.png" });
 
             // Gen 4: Sinnoh
             liste.Add(new Pokemon { Name = "Chelast", Typ = "Pflanze", HP = 55, Attack = 68, Defense = 64, Aspeed = 31, BildPfad = "chelast.png" });
@@ -129,7 +129,7 @@ namespace pokemonQuartett
 
             // Gen 5: Einall
             liste.Add(new Pokemon { Name = "Serpifeu", Typ = "Pflanze", HP = 45, Attack = 45, Defense = 55, Aspeed = 63, BildPfad = "serpifeu.png" });
-            liste.Add(new Pokemon { Name = "Floink", Typ = "Feuer", HP = 65, Attack = 63, Defense = 45, Aspeed = 45, BildPfad = "floink.png" });
+            liste.Add(new Pokemon { Name = "Floink", Typ = "Feuer", HP = 75, Attack = 63, Defense = 45, Aspeed = 35, BildPfad = "floink.png" });
             liste.Add(new Pokemon { Name = "Ottaro", Typ = "Wasser", HP = 55, Attack = 55, Defense = 45, Aspeed = 45, BildPfad = "ottaro.png" });
 
             // Gen 6: Kalos
@@ -149,15 +149,15 @@ namespace pokemonQuartett
 
             // Gen 9: Paldea
             liste.Add(new Pokemon { Name = "Felori", Typ = "Pflanze", HP = 40, Attack = 61, Defense = 54, Aspeed = 65, BildPfad = "felori.png" });
-            liste.Add(new Pokemon { Name = "Krokel", Typ = "Feuer", HP = 67, Attack = 45, Defense = 59, Aspeed = 36, BildPfad = "krokel.png" });
+            liste.Add(new Pokemon { Name = "Krokel", Typ = "Feuer", HP = 75, Attack = 45, Defense = 65, Aspeed = 30, BildPfad = "krokel.png" });
             liste.Add(new Pokemon { Name = "Kwaks", Typ = "Wasser", HP = 55, Attack = 65, Defense = 45, Aspeed = 50, BildPfad = "kwaks.png" });
 
-            // --- Die 5 Legendären (Nr. 28 bis 32) ---
-            liste.Add(new Pokemon { Name = "Mewtu", Typ = "Psycho", HP = 106, Attack = 110, Defense = 90, Aspeed = 130, BildPfad = "mewtu.png" });
-            liste.Add(new Pokemon { Name = "Groudon", Typ = "Psycho/Flug", HP = 106, Attack = 90, Defense = 130, Aspeed = 110, BildPfad = "groudon.png" });
-            liste.Add(new Pokemon { Name = "Kyogre", Typ = "Wasser", HP = 100, Attack = 100, Defense = 90, Aspeed = 90, BildPfad = "kyogre.png" });
-            liste.Add(new Pokemon { Name = "Pikachu", Typ = "Drache/Flug", HP = 60, Attack = 70, Defense = 40, Aspeed = 65, BildPfad = "pikachu.png" });
-            liste.Add(new Pokemon { Name = "Letarking", Typ = "Normal", HP = 120, Attack = 120, Defense = 120, Aspeed = 10, BildPfad = "letarking.png" });
+            // --- Die 5 Legendären (Balanciert) ---
+            liste.Add(new Pokemon { Name = "Mewtu", Typ = "Psycho", HP = 106, Attack = 110, Defense = 30, Aspeed = 130, BildPfad = "mewtu.png" });
+            liste.Add(new Pokemon { Name = "Groudon", Typ = "Boden", HP = 100, Attack = 110, Defense = 140, Aspeed = 20, BildPfad = "groudon.png" });
+            liste.Add(new Pokemon { Name = "Kyogre", Typ = "Wasser", HP = 100, Attack = 100, Defense = 60, Aspeed = 90, BildPfad = "kyogre.png" });
+            liste.Add(new Pokemon { Name = "Pikachu", Typ = "Elektro", HP = 45, Attack = 80, Defense = 40, Aspeed = 120, BildPfad = "pikachu.png" });
+            liste.Add(new Pokemon { Name = "Letarking", Typ = "Normal", HP = 150, Attack = 150, Defense = 80, Aspeed = 5, BildPfad = "letarking.png" });
 
             return liste;
         }
